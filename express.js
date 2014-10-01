60d88511cf471fe2e6c5a3f3fea27bb75c578360
angelozerr <angelo.zerr@gmail.com>
2014-10-01 17:33:54 +0200

    Upon http://expressjs.com/4x/api.html#req.query

--- express.js
+++ express.js
@@ -194,6 +194,16 @@
         "!type": "fn()",
         prototype : {
           "!proto" : "http.IncomingMessage.prototype",
+          params: {
+            "!type": "+Object",
+            "!url": "http://expressjs.com/4x/api.html#req.params",
+            "!doc": "This property is an object containing properties mapped to the named route \"parameters\". For example, if you have the route /user/:name, then the \"name\" property is available to you as req.params.name. This object defaults to {}."
+          },
+          query: {
+            "!type": "+Object",
+            "!url": "http://expressjs.com/4x/api.html#req.query",
+            "!doc": "This property is an object containing the parsed query-string, defaulting to {}."
+          },
           param: {
             "!type": "fn(name: string, defaultValue?: ?)",
             "!url": "http://expressjs.com/4x/api.html#req.param",
